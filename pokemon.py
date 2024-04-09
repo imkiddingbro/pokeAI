be438c38e2049202b9711bb1bad4645a7368e6d4
Anthony Marino <ant.marino2@gmail.com>
2024-04-09 10:56:15 -0500

    updated spacing with health

--- pokemon.py
+++ pokemon.py
@@ -44,8 +44,8 @@ class Pokemon:
                 if self.types == k:
                     # For if both pokemon are the same types
                     if Pokemon2.types == k:
-                        string_1_attack = '\nNot very effective...'
-                        string_2_attack = '\nNot very effective...'
+                        string_1_attack = '\nNot very effective...\n'
+                        string_2_attack = '\nNot very effective...\n'
 
                     # Pokemon2 is STRONG
                     if Pokemon2.types == version[(i+1)%3]:
@@ -53,8 +53,8 @@ class Pokemon:
                         Pokemon2.defense *= 2
                         self.attack /= 2
                         self.defense /= 2
-                        string_1_attack = '\nNot very effective...'
-                        string_2_attack = '\nSuper effective!!'
+                        string_1_attack = '\nNot very effective...\n'
+                        string_2_attack = '\nSuper effective!!\n'
                         
                     # Pokemon2 is WEAK
                     if Pokemon2.types == version[(i+2)%3]:
@@ -62,8 +62,8 @@ class Pokemon:
                         self.defense *= 2
                         Pokemon2.attack /= 2
                         Pokemon2.defense /= 2
-                        string_1_attack = '\nSuper Effective!!'
-                        string_2_attack = '\nNot very effective...'
+                        string_1_attack = '\nSuper Effective!!\n'
+                        string_2_attack = '\nNot very effective...\n'
                         
             while (self.bars > 0) and (Pokemon2.bars > 0):
                 print(f"\n{self.name}\t\tHLTH\t{self.health}") 
